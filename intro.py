47375a22565ae3fae72a0dbae9f5954bd5687813
mercurioregon <jamesm620@gmail.com>
2024-09-05 13:57:38 -0700

    working python workspace

---
+++ intro.py
@@ -0,0 +1,11 @@
+print ("Hello World")
+
+
+first_name = "Jim"
+last_name = " Mercurio"
+weight = 200
+sentence = first_name + last_name + " weighs" + str(weight) + " lbs."
+print (sentence)
+
+data_type = type("nine")
+print(data_type)
