69667b791c6f445797a30835bb0664afea389643
mercurioregon <jamesm620@gmail.com>
2024-09-09 19:10:09 -0700

    lists

--- intro.py
+++ intro.py
@@ -20,3 +20,13 @@ print(sentence[5:])
 sentence = "the sum of {0} + {1} is {2}".format(9, 7, 16)
 
 print(sentence)
+
+# ------
+#new_list = [4,55,21,0,88,74,121,3,5,8]
+new_list = ["butter", "cake", "dogs", "zippers", "sauce"]
+#new_list.append("Caboose")
+#new_list[3] = "Poop"
+new_list.sort()
+new_list.reverse()
+
+print(new_list)
