6a2df0ca2aa00b290b8c99cffdc1a8e0af848d91
mercurioregon <jamesm620@gmail.com>
2024-09-11 16:38:13 -0700

    tuples and dictionaries

--- intro.py
+++ intro.py
@@ -45,3 +45,29 @@ big_list = ["a", "b", "c", 2,4,5,["Monday", "Tuesday", "Wednesday"], 5, 7]
 small_list = big_list[6][2
 ]
 print(small_list)
+
+# new_list = [4,55,21,0,88,74,121,3,5,8,0,0,0,0,0,0]
+#
+# # num_pos = new_list.index(121)
+# num_pos = new_list.count(0)
+# print(num_pos)
+
+
+#TUPLES (Immutable strings, however nested strings can be changed)
+# new_tuple = (1,2,3,4, "Oregon", "Washington", ["g", "d", "h"], 5, 55, 555)
+# # new_tuple[6][1] = "x"
+#
+# print(new_tuple[7:10])
+
+#DICTIONARIES
+#Not position oriented. {Key : data/value}
+# Call the key
+#Unsortable
+#.pop remove by key
+#.clear
+
+dict = {"a" : "New York" ,"b": 1000}
+dict ["b"] = "Oregon"
+dict ["c"] = "Washington"
+print(dict)
+
